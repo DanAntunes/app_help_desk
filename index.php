a4677804b3ec5de4a5a0a72ae45d7593231dec2b
Soren <danilo-258@hotmail.com>
2025-03-03 10:38:57 -0300

    feat(index.php): adicionar folha de estilos global

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
   <title>App Help Desk</title>
+  <link rel="stylesheet" href="./src/assets/css/global/global.css">
 </head>
 <body>
   
