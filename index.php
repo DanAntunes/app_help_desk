d124cbead4c323ef22e620f3f60765ff6edacd39
Soren <danilo-258@hotmail.com>
2025-03-05 00:02:31 -0300

    refactor(index): atualizar metadados, título e links para favicons

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+  <!-- Metadados essenciais -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!-- Metadados de SEO -->
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="Danilo Antunes">
+  <!-- Framework -->
   <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <title>App Help Desk</title>
+  <!-- Folhas de estilo -->
   <link rel="stylesheet" href="./src/assets/css/style.css">
+  <!-- Favicon e Apple Touch Icons -->
+  <link rel="apple-touch-icon" sizes="180x180" href="./public/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./public/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./public/favicons/favicon-16x16.png">
+  <link rel="manifest" href="./public/favicons/site.webmanifest">
+  <!-- Título otimizado -->
+  <title>App Help Desk - Sua Central de Ajuda</title>
 </head>
 <body>
   
